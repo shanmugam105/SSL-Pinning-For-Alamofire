58f377c9be6639d0ec74dcd4ae0ea5af9bf606ea
Shanmugarajeshwaran R <shanmugarajeshwaran@sparkouttech.com>
2021-10-09 17:35:13 +0530

    Initial Commit

---
+++ SSL-Pinning-For-Alamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSL-Pinning-For-Alamofire
+//
+//  Created by Mac on 09/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
